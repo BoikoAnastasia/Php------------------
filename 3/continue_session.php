ee9f35df6a7773aec8840e1b3da32ea16a965b5b
Anastasia <anastasia151000@rambler.ru>
2022-05-05 20:11:26 +0300

    fick

--- 3/continue_session.php
+++ 3/continue_session.php
@@ -12,7 +12,8 @@ if(isset($_SESSION['forename']))
     Ваше полное имя $forename $surname . <br>";
 }
 else echo "Please, for aythentificate <a href='session.php'>Щелкните здесь</a>";
-destroy_session_and_data(){
+function destroy_session_and_data()
+{
     $_SESSION = array();
     setcookie(session_name(), '', time()- 2592000, '/');
     session_destroy();
